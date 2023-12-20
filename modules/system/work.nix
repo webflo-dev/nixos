d5a9f6b93902a6fe28b557e0f46fa74f0d383008
Florent <6934017+webflo-dev@users.noreply.github.com>
2023-12-20 16:42:05 +0100

    add package to work module: vanta-agent

--- modules/system/work.nix
+++ modules/system/work.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   environment.systemPackages = with pkgs; [
     slack
     _1password-gui
     postgresql
     nodejs_20
+    inputs.webflo.packages.${vars.system}.vanta-agent
   ];
 
 
