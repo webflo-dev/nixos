f60b19d1136560cd7d1a8d91c0e65848a19f16be
Florent <6934017+webflo-dev@users.noreply.github.com>
2024-03-19 15:01:54 +0100

    enable nvidia for docker

--- modules/system/docker/default.nix
+++ modules/system/docker/default.nix
@@ -5,7 +5,8 @@
   ...
 }: let
   cfg = config.webflo.modules.docker;
-  inherit (lib) mkEnableOption mkOption mkIf types;
+  inherit (lib) mkEnableOption mkOption mkIf types mkMerge;
+  nvidiaCfg = config.webflo.modules.nvidia;
 in {
   options.webflo.modules.docker = {
     enable = mkEnableOption "Docker module";
@@ -15,27 +16,45 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    virtualisation.docker = {
-      enable = true;
-      daemon.settings = {
-        # storage-driver = "overlay2";
-        features = {buildkit = true;};
-        experimental = true;
-        no-new-privileges = true;
-      };
-      autoPrune = {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      virtualisation.docker = {
         enable = true;
-        flags = ["--all"];
-        dates = "weekly";
+        daemon.settings = {
+          # storage-driver = "overlay2";
+          features = {buildkit = true;};
+          experimental = true;
+          no-new-privileges = true;
+        };
+        autoPrune = {
+          enable = true;
+          flags = ["--all"];
+          dates = "weekly";
+        };
       };
-    };
 
-    users.groups."docker".members = cfg.dockerGroupMembers;
+      users.groups."docker".members = cfg.dockerGroupMembers;
 
-    environment.systemPackages = with pkgs; [
-      docker
-      docker-compose
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        docker
+        docker-compose
+      ];
+    }
+
+    (mkIf nvidiaCfg.enable {
+      virtualisation.docker = {
+        # deprecated, use cdi.dynamic.nvidia.enable
+        enableNvidia = nvidiaCfg.enable;
+      };
+
+      virtualisation.containers = {
+        enable = true;
+        cdi.dynamic.nvidia.enable = nvidiaCfg.enable;
+      };
+
+      environment.systemPackages = with pkgs; [
+        nvidia-container-toolkit
+      ];
+    })
+  ]);
 }
